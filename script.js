5a64c70b352af94c81207877c474ee0acf4cde79
brzostovski <137798050+brzostovski@users.noreply.github.com>
2023-08-14 21:14:52 +0200

    Add logic to make only legal moves

--- script.js
+++ script.js
@@ -1,7 +1,55 @@
-let myModule = (function() {
-  window.addEventListener('click', click);
+const game = (function() {
+  /* Variables */
+  const cells = document.querySelectorAll('.cell');
+  const resetButton = document.querySelector('#reset');
+  let board = [];
+  let prevTurn = 'player2';
 
-  function click() {
-    console.log('click');
+  /* Bindings */
+  cells.forEach(cell => {
+    cell.addEventListener('click', () => {
+      assignValue(cell);
+      render();
+    })
+  })
+
+  resetButton.addEventListener('click', () => clearBoard());
+
+  /* Player factory */
+  const players = (function() {
+    const playerFactory = (symbol) => {
+      return {
+        symbol
+      }
+    }
+    const player1 = playerFactory('X');
+    const player2 = playerFactory('O');
+    return {
+      player1,
+      player2,
+    }
+  })()
+
+  /* Functions */
+  function assignValue(cell) {
+    if (!board[cell.dataset.index]) {
+      (prevTurn === 'player1')
+        ? (prevTurn = 'player2')
+        : (prevTurn = 'player1');
+      board[cell.dataset.index] = players[prevTurn].symbol;
+    }
   }
+
+  function render() {
+    cells.forEach(cell => {
+      cell.textContent = board[cell.dataset.index];
+    })
+  }
+
+  function clearBoard() {
+    board = [];
+    render();
+  }
+
+  return {};
 })()
