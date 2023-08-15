900cf11822a880e54c83b2a11bb2781b0bbc3e9c
brzostovski <137798050+brzostovski@users.noreply.github.com>
2023-08-15 20:01:39 +0200

    Add function to update board.arr children

--- script.js
+++ script.js
@@ -14,48 +14,48 @@ const game = (function() {
   resetButton.addEventListener('click', () => board.reset(cells));
 
   const gameController = (function() {
-    const playerFactory = (symbol, name) => {
+    const _playerFactory = (symbol, name) => {
       return {
         symbol,
         name,
       }
     }
 
-    const players = [
-      playerFactory('X', 'player1'),
-      playerFactory('O', 'player2'),
+    const _players = [
+      _playerFactory('X', 'player1'),
+      _playerFactory('O', 'player2'),
     ];
-    let activePlayer = players[0];
+    let activePlayer = _players[0];
 
     function resetActivePlayer() {
-      activePlayer = players[0];
+      activePlayer = _players[0];
     }
 
-    function checkGameState() {
-      function checkBoardFull() {
+    function _checkGameState() {
+      function _checkBoardFull() {
         for (let i = 0; i < board.arr.length; i++) {
           if (board.arr[i] === '') {return false};
         }
         return true;
       }
 
-      function checkForWinner() {
+      function _checkForWinner() {
         //find winner logic here
       }
 
-      if (checkBoardFull() || checkForWinner()) {
+      if (_checkBoardFull() || _checkForWinner()) {
         board.toggleHidden([resetButton, gameOverCard]);
       };
     }
 
     function makeMove(cell) {
       if (!board.arr[cell.dataset.index]) {
-        board.arr[cell.dataset.index] = activePlayer.symbol;
-        (activePlayer === players[0])
-          ? (activePlayer = players[1])
-          : (activePlayer = players[0]);
+        board.updateArr(cell.dataset.index, activePlayer.symbol);
+        (activePlayer === _players[0])
+          ? (activePlayer = _players[1])
+          : (activePlayer = _players[0]);
       }
-      checkGameState();
+      _checkGameState();
     }
 
     return {
@@ -74,22 +74,29 @@ const game = (function() {
     let cols = [];
     let diagons = [[], []];
 
-    for (let i = 0; i < sideLen; i++) {
-      rows[i] = [];
-      cols[i] = [];
-    };
-    
-    for (let i = 0; i < sideLen; i++) {
-      for (let j = 0; j < sideLen; j++) {
-        rows[j][i] = cells[i + (j * 3)];
-        cols[i][j] = cells[i + (j * 3)];
+    function _updateArrChildren() {  
+      for (let i = 0; i < sideLen; i++) {
+        rows[i] = [];
+        cols[i] = [];
       };
-    };
+      
+      for (let i = 0; i < sideLen; i++) {
+        for (let j = 0; j < sideLen; j++) {
+          rows[j][i] = arr[i + (j * 3)];
+          cols[i][j] = arr[i + (j * 3)];
+        };
+      };
 
-    for (let i = 0; i < sideLen; i++) {
-      diagons[0][i] = rows[i][i];
-      diagons[1][i] = rows[i][sideLen - 1 -i];
-    };
+      for (let i = 0; i < sideLen; i++) {
+        diagons[0][i] = rows[i][i];
+        diagons[1][i] = rows[i][sideLen - 1 -i];
+      };
+    }
+
+    function updateArr(index = 0, symbol = '') {
+      arr[index] = symbol;
+      _updateArrChildren();
+    }
 
     function render(cell) {
       cell.textContent = board.arr[cell.dataset.index];
@@ -105,10 +112,11 @@ const game = (function() {
 
     function reset(cells) {
       for (i = 0; i < cells.length; i++) {
-        board.arr[i] = '';
+        arr[i] = '';
         cells[i].classList.remove('populated');
         render(cells[i]);
       }
+      updateArr();
       gameController.resetActivePlayer();
       toggleHidden([resetButton, gameOverCard]);
     }
@@ -116,12 +124,17 @@ const game = (function() {
     return {
       arr,
       sideLen,
-      cols,
       rows,
+      cols,
       diagons,
+      updateArr,
       render,
       toggleHidden,
       reset,
     }
   })()
+  return {
+    gameController,
+    board,
+  }
 })()
